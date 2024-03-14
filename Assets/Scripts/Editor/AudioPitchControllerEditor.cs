862f64a6a1e05ae9e26f002a5c23992c8fde3426
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2024-03-14 16:01:29 -0600

    feat: Added Audio Pitch Controller
    Randomize, Increase, Decrease audio pitch

---
+++ Assets/Scripts/Editor/AudioPitchControllerEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Utilities;
+
+[CustomEditor(typeof(AudioPitchController))]
+public class AudioPitchControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var t = (AudioPitchController)target;
+
+        t.pitchType = (PitchType)EditorGUILayout.EnumPopup("Pitch Type", t.pitchType);
+
+        switch (t.pitchType)
+        {
+            case PitchType.Random:
+                t.minPitch = EditorGUILayout.Slider("Min Pitch", t.minPitch, -3.0f, 3.0f);
+                t.maxPitch = EditorGUILayout.Slider("Max Pitch", t.maxPitch, -3.0f, 3.0f);
+                // EditorGUILayout.MinMaxSlider(ref t.minPitch, ref t.maxPitch, -3.0f, 3.0f);
+                break;
+            case PitchType.Increase:
+            case PitchType.Decrease:
+                t.pitchStep = EditorGUILayout.Slider("Pitch Step", t.pitchStep, 0.1f, 2.0f);
+                break;
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(t);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/AudioPitchController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioPitchController : MonoBehaviour
+    {
+        public PitchType pitchType = PitchType.Random;
+        
+        [Tooltip("The minimum pitch value")]
+        [Range(-3.0f, 3.0f)]
+        public float minPitch = 0.5f;
+        
+        [Tooltip("The maximum pitch value")]
+        [Range(-3.0f, 3.0f)]
+        public float maxPitch = 3.0f;
+        
+        [Tooltip("The pitch value to increase or decrease by")]
+        [Range(0.1f, 2.0f)]
+        public float pitchStep = 0.1f;
+        
+        private AudioSource _audioSource;
+        private float _currentPitch;
+        
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _currentPitch = _audioSource.pitch;
+        }
+
+        private void OnEnable() => AdjustAudioPitch();
+
+        private void AdjustAudioPitch()
+        {
+            switch (pitchType)
+            {
+                case PitchType.Random:
+                    _audioSource.pitch = Random.Range(minPitch, maxPitch);
+                    break;
+                case PitchType.Increase:
+                    _currentPitch += pitchStep;
+                    if (_currentPitch > maxPitch)
+                        _currentPitch = maxPitch;
+                    _audioSource.pitch = _currentPitch;
+                    break;
+                case PitchType.Decrease:
+                    _currentPitch -= pitchStep;
+                    if (_currentPitch < minPitch)
+                        _currentPitch = minPitch;
+                    _audioSource.pitch = _currentPitch;
+                    break;
+                default:
+                    _currentPitch = _audioSource.pitch;
+                    break;
+            }
+        }
+    }
+    
+    public enum PitchType
+    {
+        Random,
+        Increase,
+        Decrease
+    }
+}
