9c172b5ba2f088d744f14bada82b8c05638c361f
Percy <perzeus.ttv@gmail.com>
2024-03-29 11:38:29 -0600

    feat: Moving Door Test and Gizmo

--- Assets/Scripts/World/Environmental/MovingDoor.cs
+++ Assets/Scripts/World/Environmental/MovingDoor.cs
@@ -13,7 +13,8 @@ namespace World.Environmental
         [SerializeField] private float openTime = 2.0f;
         [SerializeField] private List<Deposit> deposits;
         [SerializeField] private AudioDataEnumSoundFx openSoundFx;
-        
+        [SerializeField] private bool testOpenDoor;
+
         private AudioSource _audioSource;
         private Vector3 _closedPosition;
         private bool _isOpen;
@@ -26,7 +27,7 @@ namespace World.Environmental
         private void Start()
         {
             _closedPosition = transform.localPosition;
-            
+
             foreach (var deposit in deposits)
                 deposit.onDepositMade.AddListener(CheckDeposits);
         }
@@ -37,13 +38,25 @@ namespace World.Environmental
                 deposit.onDepositMade.RemoveListener(CheckDeposits);
         }
 
+        private void Update()
+        {
+            if (testOpenDoor && !_isOpen)
+            {
+                StartCoroutine(OpenDoor());
+            }
+            else if (!testOpenDoor && _isOpen)
+            {
+                StartCoroutine(CloseDoor());
+            }
+        }
+
         private void CheckDeposits()
         {
             if (deposits.Any(deposit => !deposit.hasDeposited)) return;
 
             if (!_isOpen) StartCoroutine(OpenDoor());
         }
-        
+
         private IEnumerator OpenDoor()
         {
             _audioSource.PlayOneShot(openSoundFx, true);
@@ -55,8 +68,37 @@ namespace World.Environmental
                 transform.localPosition = Vector3.Lerp(_closedPosition, openLocalPosition, t / openTime);
                 yield return null;
             }
-            
-            transform.localPosition = openLocalPosition;
+        }
+
+        private IEnumerator CloseDoor()
+        {
+            _audioSource.PlayOneShot(openSoundFx, true);
+            var t = 0.0f;
+            var startingPos = transform.localPosition;
+            while (t < openTime)
+            {
+                t += Time.deltaTime;
+                transform.localPosition = Vector3.Lerp(startingPos, _closedPosition, t / openTime);
+                yield return null;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Vector3 direction = openLocalPosition - _closedPosition;
+            Vector3 start = transform.position;
+            Vector3 end = start + direction;
+
+            Gizmos.DrawLine(start, end);
+
+            float arrowHeadLength = 0.25f;
+            float arrowHeadAngle = 20.0f;
+
+            Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+            Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+            Gizmos.DrawRay(end, right * arrowHeadLength);
+            Gizmos.DrawRay(end, left * arrowHeadLength);
         }
     }
 }
