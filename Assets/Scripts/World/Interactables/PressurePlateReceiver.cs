e7c2433b50d3c68a77a5a5deb3735ae5f7645a8d
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2024-03-31 03:08:36 -0600

    feat: Pressure Plate

---
+++ Assets/Scripts/World/Interactables/PressurePlateReceiver.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace World.Interactables
+{
+    public class PressurePlateReceiver : MonoBehaviour
+    {
+        public void Activate()
+        {
+            Debug.Log("Pressure plate activated!");
+        }
+        
+        public void Deactivate()
+        {
+            Debug.Log("Pressure plate deactivated!");
+        }
+    }
+}
