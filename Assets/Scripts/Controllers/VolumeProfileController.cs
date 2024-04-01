0b8b43965008796fe911d9d241da7fa38e7c83a1
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2024-03-31 23:26:28 -0600

    feat: Refactor VolumeProfileController & Improve logic

--- Assets/Scripts/Controllers/VolumeProfileController.cs
+++ Assets/Scripts/Controllers/VolumeProfileController.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System.Collections;
+using JetBrains.Annotations;
 using Managers;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -13,11 +14,8 @@ namespace Controllers
         [ReadOnly] public bool isNightVisionActive;
         
         private PlayerController _player;
-        private VolumeProfile _tempProfile;
-        private VolumeProfile _nightVisionProfile;
-        private VolumeProfile _targetProfile;
-        private bool _isTransitioning;
-        private float _timeElapsed;
+        private VolumeProfile _tempProfile, _nightVisionProfile, _targetDefaultProfile, _targetNightVisionProfile;
+        private Coroutine _transitionCoroutine, _nightVisionStartCoroutine;
         private bool _didSwitchProfile;
         
         private void Awake()
@@ -26,20 +24,19 @@ namespace Controllers
             
             if (globalVolume == null)
                 Debug.LogError("Global Volume is not assigned!");
+            
+            // Create a temporary profile to store the interpolated values.
+            _tempProfile = ScriptableObject.CreateInstance<VolumeProfile>();
+            _tempProfile.name = "Default Temp Profile";
+            
+            // Create a night vision profile. This is a separate profile that will be used when the player equips the night vision.
+            _nightVisionProfile = ScriptableObject.CreateInstance<VolumeProfile>();
+            _nightVisionProfile.name = "Night Vision Temp Profile";
         }
 
         private void Start()
         {
-            // Create a temporary profile to store the interpolated values.
-            _tempProfile = ScriptableObject.CreateInstance<VolumeProfile>();
-            _tempProfile.name = "Temp Profile";
             CopyProfile(_player.settings.sanityProfile100, _tempProfile);
-            _targetProfile = _tempProfile;
-            globalVolume.profile = _tempProfile;
-            
-            // Create a night vision profile. This is a separate profile that will be used when the player equips the night vision.
-            _nightVisionProfile = ScriptableObject.CreateInstance<VolumeProfile>();
-            _nightVisionProfile.name = "Night Vision Profile";
             CopyProfile(_player.settings.nightVisionProfile, _nightVisionProfile);
         }
 
@@ -50,42 +47,60 @@ namespace Controllers
             if (isNightVisionActive)
             {
                 globalVolume.profile = _nightVisionProfile;
-                _didSwitchProfile = true;
-            }
-            else
-            {
-                if (_didSwitchProfile)
+                if (!_didSwitchProfile)
                 {
-                    globalVolume.profile = _tempProfile;
-                    _didSwitchProfile = false;
-                }
-                
-                if (_isTransitioning)
-                {
-                    _timeElapsed += Time.deltaTime;
-                    var t = Mathf.Clamp01(_timeElapsed / transitionTime);
-                    InterpolateProfile(_tempProfile, _targetProfile, t);
-                    
-                    if (t >= 1f)
-                        _isTransitioning = false;
+                    // At the start of the night vision, Lerp the Post Exposure value from 3 to 0 over 3 seconds.
+                    StartPostExposureLerp(_nightVisionProfile, 3.0f, 0, 3.0f);
+                    _didSwitchProfile = true;
                 }
                 else
                 {
-                    HandleVolumeEffects();
+                    _nightVisionStartCoroutine = null;
                 }
+                
+                HandleNightVisionProfile();
+            }
+            else
+            {
+                globalVolume.profile = _tempProfile;
+                _didSwitchProfile = false;
+                HandleSanityProfile();
             }
         }
-
+        
         private void OnDestroy()
         {
             CleanUpProfile(_tempProfile);
             CleanUpProfile(_nightVisionProfile);
         }
-
-        private void HandleVolumeEffects()
+        
+        private void HandleSanityProfile()
         {
             var sanityPercentage = _player.CurrentSanity / _player.settings.maxSanity * 100.0f;
-            var newTargetProfile = sanityPercentage switch
+            var newTargetProfile = GetSanityProfile(sanityPercentage);
+            
+            if (_targetDefaultProfile != newTargetProfile)
+            {
+                _targetDefaultProfile = newTargetProfile;
+                StartProfileTransition(_tempProfile, _targetDefaultProfile, transitionTime);
+            }
+        }
+        
+        private void HandleNightVisionProfile()
+        {
+            var sanityPercentage = _player.CurrentSanity / _player.settings.maxSanity * 100.0f;
+            var newTargetProfile = GetSanityProfile(sanityPercentage);
+
+            if (_targetNightVisionProfile != newTargetProfile)
+            {
+                _targetNightVisionProfile = newTargetProfile;
+                StartProfileTransition(_nightVisionProfile, _targetNightVisionProfile, transitionTime, true);
+            }
+        }
+        
+        private VolumeProfile GetSanityProfile(float sanityPercentage)
+        {
+            return sanityPercentage switch
             {
                 <= 100 and > 75 => _player.settings.sanityProfile100,
                 <= 75 and > 50 => _player.settings.sanityProfile75,
@@ -94,12 +109,59 @@ namespace Controllers
                 <= 10 and > 0 => _player.settings.sanityProfile10,
                 _ => null
             };
+        }
+        
+        private void StartProfileTransition(VolumeProfile from, VolumeProfile targetProfile, float duration, bool isNightVision = false)
+        {
+            if (_transitionCoroutine != null)
+                StopCoroutine(_transitionCoroutine);
             
-            if (newTargetProfile != _targetProfile)
+            _transitionCoroutine = StartCoroutine(TransitionProfile(from, targetProfile, duration, isNightVision));
+        }
+        
+        private IEnumerator TransitionProfile(VolumeProfile from, VolumeProfile targetProfile, float duration, bool isNightVision = false)
+        {
+            var elapsedTime = 0.0f;
+            while (elapsedTime < duration)
             {
-                _targetProfile = newTargetProfile;
-                _isTransitioning = true;
-                _timeElapsed = 0;
+                // Calculate the interpolation value.
+                var t = elapsedTime / duration;
+                
+                // Interpolate the values of the profiles.
+                InterpolateProfile(from, targetProfile, t, isNightVision);
+                
+                // Increment the elapsed time.
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            
+            // Set the global volume profile to the target profile.
+            CopyProfile(from, _tempProfile);
+        }
+        
+        private void StartPostExposureLerp(VolumeProfile profile, float start, float end, float duration)
+        {
+            if (_nightVisionStartCoroutine != null)
+                StopCoroutine(_nightVisionStartCoroutine);
+            
+            _nightVisionStartCoroutine = StartCoroutine(LerpPostExposure(profile, start, end, duration));
+        }
+        
+        private IEnumerator LerpPostExposure(VolumeProfile profile, float start, float end, float duration)
+        {
+            var elapsedTime = 0.0f;
+            while (elapsedTime < duration)
+            {
+                // Calculate the interpolation value.
+                var t = elapsedTime / duration;
+                
+                // Lerp the post exposure value.
+                if (profile.TryGet<ColorAdjustments>(out var profileColor))
+                    profileColor.postExposure.Interp(start, end, t);
+                
+                // Increment the elapsed time.
+                elapsedTime += Time.deltaTime;
+                yield return null;
             }
         }
 
@@ -109,7 +171,28 @@ namespace Controllers
         /// <param name="from">The profile to interpolate from.</param>
         /// <param name="to">The profile to interpolate to.</param>
         /// <param name="t">The interpolation value.</param>
-        private static void InterpolateProfile(VolumeProfile from, VolumeProfile to, float t)
+        /// <param name="isNightVision">Whether the night vision profile is being used.</param>
+        private static void InterpolateProfile(VolumeProfile from, VolumeProfile to, float t, bool isNightVision)
+        {
+            InterpolateVignette(from, to, t);
+            
+            InterpolateFilmGrain(from, to, t);
+
+            if (isNightVision)
+            {
+                if (from.TryGet<ColorAdjustments>(out var fromColor))
+                {
+                    fromColor.contrast.value = 0;
+                    fromColor.saturation.value = 0;
+                }
+            }
+            else
+            {
+                InterpolateColorAdjustments(from, to, t);
+            }
+        }
+
+        private static void InterpolateVignette(VolumeProfile from, VolumeProfile to, float t)
         {
             if (from.TryGet<Vignette>(out var fromVignette) && to.TryGet<Vignette>(out var toVignette))
             {
@@ -121,14 +204,20 @@ namespace Controllers
                 fromVignette.roundness.Interp(fromVignette.roundness.value, toVignette.roundness.value, t);
                 fromVignette.rounded.value = fromVignette.rounded.value;
             }
-            
+        }
+        
+        private static void InterpolateFilmGrain(VolumeProfile from, VolumeProfile to, float t)
+        {
             if (from.TryGet<FilmGrain>(out var fromFilmGrain) && to.TryGet<FilmGrain>(out var toFilmGrain))
             {
                 fromFilmGrain.type.value = fromFilmGrain.type.value;
                 fromFilmGrain.intensity.Interp(fromFilmGrain.intensity.value, toFilmGrain.intensity.value, t);
                 fromFilmGrain.response.Interp(fromFilmGrain.response.value, toFilmGrain.response.value, t);
             }
-            
+        }
+
+        private static void InterpolateColorAdjustments(VolumeProfile from, VolumeProfile to, float t)
+        {
             if (from.TryGet<ColorAdjustments>(out var fromColor) && to.TryGet<ColorAdjustments>(out var toColor))
             {
                 fromColor.postExposure.Interp(fromColor.postExposure.value, toColor.postExposure.value, t);
